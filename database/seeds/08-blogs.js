60472ea605853cf6f2566dc625537014b5fb9daa
Caleb Redd <caleb.redd@yahoo.com>
2019-12-20 02:01:06 -0700

    Case sensitive error

--- database/seeds/08-blogs.js
+++ database/seeds/08-blogs.js
@@ -3,97 +3,97 @@ exports.seed = function(knex, Promise) {
   return knex("blogs").insert([
     {
       farmer_id: 1,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This guy is a great farmer!"
     },
     {
       farmer_id: 1,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This guy is a bad farmer!"
     },
     {
       farmer_id: 1,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This guy is an okay farmer!"
     },
     {
       farmer_id: 1,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This guy is a great farmer!"
     },
     {
       farmer_id: 2,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This dog is a bad farmer!"
     },
     {
       farmer_id: 2,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This dog is an okay farmer!"
     },
     {
       farmer_id: 2,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This dog is a great farmer!"
     },
     {
       farmer_id: 2,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This dog is a bad farmer!"
     },
     {
       farmer_id: 3,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This lady is an okay farmer!"
     },
     {
       farmer_id: 3,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This lady is a great farmer!"
     },
     {
       farmer_id: 3,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This lady is a bad farmer!"
     },
     {
       farmer_id: 3,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This lady is an okay farmer!"
     },
     {
       farmer_id: 4,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This snake is a great farmer!"
     },
     {
       farmer_id: 4,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This snake is a bad farmer!"
     },
     {
       farmer_id: 4,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This snake is an okay farmer!"
     },
     {
       farmer_id: 4,
-      blogPostImgUrl:
+      blogPostImgURL:
         "https://images.unsplash.com/photo-1572731410363-1e08a28c8114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9",
       post: "This snake is a cool farmer!"
     }
